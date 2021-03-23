d05485390689c107f6e4b360b16d21fde56de95f
grey1808 <44221929+grey1808@users.noreply.github.com>
2021-03-23 16:27:06 +0300

    Update db.php

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=10.26.1.100;dbname=invent',
+    'dsn' => 'mysql:host=192.168.1.100;dbname=invent',
     'username' => 'invent',
-    'password' => '[thdgecnsyt',
+    'password' => ',fyfy',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
